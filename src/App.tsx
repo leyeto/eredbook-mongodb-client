b0e57e0896366473ea0124c0bd0e6a47ef8302f0
Adeleye Ifaturoti <adeleyeifaturoti@yahoo.com>
2023-04-08 14:24:38 +0100

    add sass and sass folders

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import "./sass/main.scss";
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Header />
-    </div>
+    </>
   );
 }
 
---
+++ src/__tests__/App_testing_old.tsx
@@ -0,0 +1,21 @@
+import { describe, it, expect } from "vitest";
+import { render, screen } from "@testing-library/react";
+import App from "../App";
+
+describe("Renders main page correctly ", () => {
+  it("Should render this page correctly", () => {
+    render(<App />);
+
+    const h1 = screen.queryByText("Vite + React");
+
+    expect(h1).not.toBeNull();
+  });
+
+  it("Button exist", () => {
+    const { container } = render(<App />);
+
+    const button = container.getElementsByClassName("card");
+
+    expect(button).toBeTruthy();
+  });
+});
